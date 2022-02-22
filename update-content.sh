82b6da9d90bc8be1d43db276b1d66f9e92ac3973
Obed N Munoz <obed.n.munoz@gmail.com>
2022-02-21 21:20:25 -0600

    Add update content script
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

---
+++ update-content.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+github_prefix=https://github.com/CodersSquad
+projects=(hello-gophers hello-c-world)
+
+for proj in $projects; do
+    git clone $github_prefix/$proj
+    pushd $proj
+    make
+    popd
+    rm -rf content/$proj
+    mv -f $proj/public content/$proj
+    rm -rf $proj
+done
