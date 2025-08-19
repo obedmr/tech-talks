3452955b538cd691e638c119f385a48ff91416e0
Obed Munoz <obed.munoz@oracle.com>
2025-08-19 14:37:16 -0600

    Update on Tue Aug 19 14:37:16 CST 2025
    Signed-off-by: Obed Munoz <obed.munoz@oracle.com>

---
+++ content/data-structures/data-abstraction/data_structs_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numbers[5] = {10, 20, 30, 40, 50};
+
+    cout << "All elements of the array:" << endl;
+    for (int i = 0; i < 5; ++i) {
+        cout << "Element at index " << i << ": " << numbers[i] << endl;
+    }
+
+    // Modifying numbers array elements, doubling their values
+    // your-code-here
+
+
+    // Verify that numbers array is updated
+    // your-code-here
+
+    return 0;
+}
